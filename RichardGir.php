3f4b2af6b75c6b1fecc88b1ab5d5dbf15b942b8b
Alexandr Bobylev <alexsbobylev@gmail.com>
2013-05-30 17:34:40 +1000

    Merge branch 'newfix'
    Conflicts:
    	demo.html

---
+++ RichardGir.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: alexanderbobylev
+ * Date: 30.05.13
+ * Time: 17:32
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Main;
+
+
+class RichardGir {
+
+    static function testClass(){
+
+        echo "Это тестовый класс";
+
+    }
+
+}
+
+?>
